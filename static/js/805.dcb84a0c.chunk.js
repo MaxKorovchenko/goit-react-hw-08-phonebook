0c22d29742e4e5bb3198ef6baac884aea00766f6
MaxKorovchenko <109962117+MaxKorovchenko@users.noreply.github.com>
2023-04-13 12:08:47 +0000

    Deploying to gh-pages from @ MaxKorovchenko/goit-react-hw-08-phonebook@339f68effd0aa864e42ce4ed297a0a06c2909c11 🚀

---
+++ static/js/805.dcb84a0c.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[805],{805:function(e,t,n){n.r(t);var r=n(184);t.default=function(){return(0,r.jsx)("main",{children:(0,r.jsx)("h2",{style:{display:"flex",justifyContent:"center",fontSize:40},children:"404 Not Found"})})}}}]);
+//# sourceMappingURL=805.dcb84a0c.chunk.js.map
