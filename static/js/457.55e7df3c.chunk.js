3cec0a35c5bbd717869a28cfd41c27a110f72f33
MaxKorovchenko <109962117+MaxKorovchenko@users.noreply.github.com>
2023-04-14 19:00:02 +0000

    Deploying to gh-pages from @ MaxKorovchenko/goit-react-hw-08-phonebook@6a08e1f8dbcc5480b851a65eb7efd8e2ce383c87 🚀

--- static/js/457.55e7df3c.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[457],{8523:function(e,t,n){n.r(t),n.d(t,{default:function(){return r}});var a=n.p+"static/media/phonebook.b9df28a0409dbfc5f8cd.jpg",c=n(184),i={main:{height:"92vh",backgroundImage:"url(".concat(a,")")},title:{textAlign:"center",padding:80,margin:0,fontSize:50,color:"white"}},r=function(){return(0,c.jsx)("main",{style:i.main,children:(0,c.jsx)("h1",{style:i.title,children:"Welcome to the PHONEBOOK APP"})})}}}]);
-//# sourceMappingURL=457.55e7df3c.chunk.js.map
